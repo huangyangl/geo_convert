47290bf0b241ff991bbebf609b4e9d0c4d86d190
huangyangl <huangyangly@126.com>
2024-09-09 16:15:40 +0800

    1

--- 1.py
+++ 1.py
@@ -28,13 +28,16 @@ with open(filename_ovjsn, 'r',encoding='utf-8-sig') as file:
     # 现在data是一个Python字典，你可以像操作普通字典一样操作它
     Latlng_list=data['ObjItems'][0]['Object']['ObjectDetail']['Latlng']
     print('Latlng_list:\n' + str(Latlng_list) + '\n')
-    prefix="1  0  3  16  0  0  0  0  "
     postfix="  1"
     alt_rel=120
+    seq=1
     # 遍历
     for i in range(0, len(Latlng_list), 2):
+        print(seq)
         lat_gcj02=Latlng_list[i]
         lng_gcj02=Latlng_list[i+1]
         lng, lat = gcj02_to_wgs84(lng_gcj02,lat_gcj02)
         print('lat_gcj02:' + str(lat_gcj02) + ", lat_gcj02:" + str(lng_gcj02) + ";   lat_wgs84:" + str(lat) + ", lng_wgs84:"+str(lng)+"\n")
+        prefix=str(seq) + "  0  3  16  0  0  0  0  "
         write_item(lat,lng,alt_rel,prefix,postfix,filename)
+        seq=seq+1
